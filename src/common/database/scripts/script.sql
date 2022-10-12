a7f7b7a850b98a956c6197793e8d682ed2cbc90d
Bryan Arévalo <barevalo@solcomp.com>
2022-10-12 15:23:00 -0600

    :rocket: Agregada la normalización de roles

--- src/common/database/scripts/script.sql
+++ src/common/database/scripts/script.sql
@@ -2,13 +2,22 @@ create database prestamos;
 
 use prestamos;
 
+create table roles (
+    rol_id                  int not null primary key auto_increment,
+    nombre                  varchar(200) not null unique 
+);
+
 create table usuarios(
     usuario_id          int not null primary key auto_increment,
     nombre              varchar(200) not null,
     correo              varchar(200) not null unique,
     password            varchar(200) not null,
-    rol                 int not null default 1
+    rol                 int not null default 1,
+
+    foreign key (rol) references roles(rol_id)
 );
+
+
 
 create table clientes(
     cliente_id          int not null primary key auto_increment,

a35fed884defaa20b9f0c21d633d11b115a0f10c
Bryan Arévalo <barevalo@solcomp.com>
2022-10-12 14:53:45 -0600

    :rocket: Agregada la tabla de clientes

--- src/common/database/scripts/script.sql
+++ src/common/database/scripts/script.sql
@@ -9,3 +9,13 @@ create table usuarios(
     password            varchar(200) not null,
     rol                 int not null default 1
 );
+
+create table clientes(
+    cliente_id          int not null primary key auto_increment,
+    nombre              varchar(200) not null,
+    correo              varchar(200) not null unique,
+    telefono            varchar(10) not null, 
+    prestamista_id      int not null,
+
+    foreign key (prestamista_id) references usuarios(usuario_id)
+);

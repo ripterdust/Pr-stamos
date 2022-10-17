e55a3ae9a0aeb49e49d4a3175d57b30e9ec4bb66
lighnio <charlesvas16@outlook.com>
2022-10-17 13:53:00 -0600

    :bulb: cambio en la respuesta si un elemento llega como indefinido

--- src/common/config/serverConfig.ts
+++ src/common/config/serverConfig.ts
@@ -2,17 +2,17 @@ import {config} from 'dotenv'
 config()
 
 interface ServerConfig{
-    PORT: string | undefined,
-    DB_NAME: string | undefined,
-    DB_USER: string | undefined,
-    DB_PASS: string | undefined,
-    DB_HOST: string | undefined
+    PORT: string,
+    DB_NAME: string,
+    DB_USER: string,
+    DB_PASS: string,
+    DB_HOST: string
 }
 
 export const serverConfig : ServerConfig = {
-    PORT: process.env.PORT,
-    DB_NAME: process.env.DB_NAME,
-    DB_USER: process.env.DB_USER,
-    DB_PASS: process.env.DB_PASS,
-    DB_HOST: process.env.DB_HOST
+    PORT: process.env.PORT || "",
+    DB_NAME: process.env.DB_NAME || "",
+    DB_USER: process.env.DB_USER || "",
+    DB_PASS: process.env.DB_PASS || "",
+    DB_HOST: process.env.DB_HOST || ""
 }

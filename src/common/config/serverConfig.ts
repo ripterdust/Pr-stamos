058548c20e241fecd550e6d988e6211b83221074
Ripterdust <bryantello2010@hotmail.com>
2022-11-04 12:50:53 -0600

    Configurado el serverConfig para que se elija el localhost de manera predeterminada

--- src/common/config/serverConfig.ts
+++ src/common/config/serverConfig.ts
@@ -1,18 +1,18 @@
-import {config} from 'dotenv'
+import { config } from 'dotenv'
 config()
 
-interface ServerConfig{
-    PORT: string,
-    DB_NAME: string,
-    DB_USER: string,
-    DB_PASS: string,
+interface ServerConfig {
+    PORT: string
+    DB_NAME: string
+    DB_USER: string
+    DB_PASS: string
     DB_HOST: string
 }
 
-export const serverConfig : ServerConfig = {
-    PORT: process.env.PORT || "",
-    DB_NAME: process.env.DB_NAME || "",
-    DB_USER: process.env.DB_USER || "",
-    DB_PASS: process.env.DB_PASS || "",
-    DB_HOST: process.env.DB_HOST || ""
+export const serverConfig: ServerConfig = {
+    PORT: process.env.PORT || '',
+    DB_NAME: process.env.DB_NAME || '',
+    DB_USER: process.env.DB_USER || '',
+    DB_PASS: process.env.DB_PASS || '',
+    DB_HOST: process.env.DB_HOST || 'localhost',
 }

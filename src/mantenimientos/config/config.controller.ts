32cd68f4eae4d46c8d16f6e346dd974075ea04ab
Ripterdust <bryantello2010@hotmail.com>
2022-11-14 19:54:07 -0600

    :rocket: Agregada la capacidad de agregar y eliminar dinero en la caja

--- src/mantenimientos/config/config.controller.ts
+++ src/mantenimientos/config/config.controller.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express'
 import Controller from '../../common/controller'
 import { ConfigModel } from './config.model'
 
@@ -6,5 +7,17 @@ export class ConfigController extends Controller {
     constructor() {
         super()
         this.modelo = new ConfigModel()
+    }
+
+    public async modificarCaja(req: Request) {
+        const dinero = parseInt(req.body.caja)
+        console.log(dinero)
+        const respuesta = await this.modelo.obtenerPorId(1)
+        const { caja } = respuesta.data[0]
+
+        const dineroTotal = caja + dinero
+
+        const consulta = await this.modelo.actualizarPorId(1, { caja: dineroTotal })
+        return consulta
     }
 }
--- src/mantenimientos/config/config.model.ts
+++ src/mantenimientos/config/config.model.ts
@@ -8,4 +8,11 @@ export class ConfigModel extends Model {
 
         this.idTabla = 'config_id'
     }
+
+    public async modificarCaja(cantidad: number) {
+        const pool = await this.connection.getConnection(this.nombreConexion)
+        let consulta = pool!.select('*').from('config').where('id', 1)
+
+        console.log(consulta)
+    }
 }

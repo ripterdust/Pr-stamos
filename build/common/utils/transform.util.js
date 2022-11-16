2cdc0ff6a9729eec474069f6053f16b35ebe6ca6
ripterdust <bryantello2010@hotmail.com>
2022-11-15 18:55:11 -0600

    :rocket: Eliminado el build

--- build/common/utils/transform.util.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseToJson = void 0;
-const parseToJson = (element) => JSON.parse(JSON.stringify(element));
-exports.parseToJson = parseToJson;
